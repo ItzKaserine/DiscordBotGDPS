558dc39fc45b5fd8ea7d4b50865f3819916c3127
KaserineGD <45907027+KaserineGD@users.noreply.github.com>
2021-01-10 19:05:31 +0900

    Update status.php

--- bot/status.php
+++ bot/status.php
@@ -1,7 +1,7 @@
 <?php
 include "../config/connection.php";
 try {
-	$db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	$db = new PDO("mysql:host=gdps_kaserine;dbname=gdps_kaserine", gdps_kaserine, B2kL372IpG5e8ehUC9lLdd);
 	//works fine
 	exit("1");
 	} catch (PDOException $e){
